0f526382423c184a9032026c210ce352530ea249
Moti Korets <moti.korets@clicksoftware.com>
2014-11-05 01:03:38 +0200

    remove all + standalone monkey

--- monkey/sfdc.debug.btn.js
+++ monkey/sfdc.debug.btn.js
@@ -1,34 +1,57 @@
+// ==UserScript==
+// @name         Add Salesforce Debug Log Buttons
+// @namespace    sfdc
+// @version      0.1
+// @description  Add Salesforce Debug Log Buttons
+// @author       You
+// @match       https://*.salesforce.com/setup/ui/listApexTraces.apexp*
+// @require     file:///c:/sfdc-debug-logs/monkey/sfdc.debug.btn.js
+// @grant unsafeWindow
+// @run-at document-end
+// ==/UserScript==
+
 (function(){
-/*var deleteAllLogsApex = ['List<ApexLog> allLogs = [Select Id From ApexLog];',
-                        'delete allLogs;'].join('\n');*/
 var sid = document.cookie.match(/(^|;\s*)sid=(.+?);/)[2];
 var userName = document.getElementById('userNavLabel').innerText;
 var form = document.getElementById("Apex_Trace_List:monitoredUsersForm");
 var pbButton = form.getElementsByClassName('pbButton');
 var deleteAllContainer = document.getElementById("Apex_Trace_List:traceForm").getElementsByClassName('pbButton')[0];
-
 var addUserButton = document.createElement('button');
 var realDeleteAll = document.createElement('button');
 realDeleteAll.innerText = 'Delete All (for real)';
 realDeleteAll.onclick = function(event){
     event.preventDefault();
     var xhr = new XMLHttpRequest();
+   /* var apexToDel = 'delete [Select Id From ApexLog];';
+    xhr.open('GET','/services/data/v30.0/tooling/executeAnonymous/?anonymousBody=' + encodeURIComponent(apexToDel));
+    xhr.onload = function(result){
+        console.log(this.responseText);
+        debugger;
+         window.location.href = window.location.href;
+    }*/
+
     xhr.open('GET','/services/data/v30.0/tooling/query/?q=' + encodeURIComponent('Select Id From ApexLog'),true);
     xhr.onload = function(result){
-        debugger;
-        console.log(this.responseText);
         var reponseObj = JSON.parse(this.responseText);
         var logIds = reponseObj.records.map(function(logObj){
             return logObj.Id;
         });
         console.log(logIds.length);
+        var logsCounter = logIds.length;
         logIds.map(function(id){
             var deleteLogs = new XMLHttpRequest();
             deleteLogs.open('DELETE','/services/data/v30.0/tooling/sobjects/ApexLog/' + id,true);
             deleteLogs.setRequestHeader('Authorization','Bearer ' + sid);
+            deleteLogs.onload = function(){
+                logsCounter--;
+            }
             deleteLogs.send();
         });
-        window.location.href = window.location.href;
+        setInterval(function(){
+            if(logsCounter == 0){
+                window.location.href = window.location.href;
+            }
+        },1000);
     }
     xhr.setRequestHeader('Authorization','Bearer ' + sid);
     xhr.send();
@@ -64,16 +87,4 @@ addUserButton.onclick = function(event){
     window.location.href = window.location.href;
 }
 pbButton[0].appendChild(addUserButton);
-
-function readCookie(name) {
-    var nameEQ = name + "=";
-    var ca = document.cookie.split(';');
-    for(var i=0;i < ca.length;i++) {
-        var c = ca[i];
-        while (c.charAt(0)==' ') c = c.substring(1,c.length);
-        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-    }
-    return null;
-}
-
 })();
--- monkey/sfdc.debug.js
+++ monkey/sfdc.debug.js
@@ -1,9 +1,25 @@
+// ==UserScript==
+// @name         Beautify Salesforce Debug View
+// @namespace    SFDC
+// @version      0.1
+// @description
+// @author       Moti
+// @match        https://*.salesforce.com/p/setup/layout/ApexDebugLogDetailEdit/*
+// @grant        none
+// @require file:///c:/sfdc-debug-logs/monkey/beautify.js
+// @require file:///c:/sfdc-debug-logs/monkey/sfdc.debug.js
+// @resource debug_css file:///c:/sfdc-debug-logs/monkey/debug.css
+// @grant    GM_addStyle
+// @grant    GM_getResourceText
+// @run-at document-end
+// ==/UserScript==
 
 var debug_css = GM_getResourceText ("debug_css");
 GM_addStyle (debug_css);
+var showSystemLabel = document.createElement('label');
+showSystemLabel.innerHTML = '<input type="checkbox" name="checkbox" value="value" id="showSystem" >Show System Methods</label>';
 var codeElement = document.getElementsByTagName('pre')[0];
 var debugText = codeElement.innerText;
-
 var res = debugText.split('\n').map(function(curLine){
     var splitedDebugLine = curLine.split('|');
     if(curLine.indexOf('Execute Anonymous:') == 0){
@@ -16,7 +32,7 @@ var res = debugText.split('\n').map(function(curLine){
         return '<div class="o debug">' +  curLine + '</div>';
     }
     if(curLine.indexOf('|SYSTEM_METHOD_') > -1){
-        return '<div class="c">' +  curLine + '</div>';
+        return '<div class="c systemMethodLog">' +  curLine + '</div>';
     }
     if(curLine.indexOf('|SOQL_EXECUTE_') > -1){
         return '<div class="l">' +  curLine + '</div>';
@@ -40,17 +56,47 @@ var res = debugText.split('\n').map(function(curLine){
     return prevVal.substr(0,prevVal.length - '</div>'.length) + '\n' + curLine + '</div>';
 
 });
-document.getElementsByTagName('pre')[0].innerHTML = '<div class="hll">' + res + '</div>';
+document.getElementsByTagName('pre')[0].innerHTML = '<div class="hll" id="debugText">' + res + '</div>';
 document.getElementsByClassName('oLeft')[0].style.display ="none";
 var userDebugDivs = document.getElementsByClassName('debug');
+document.getElementsByClassName('codeBlock')[0].insertBefore(showSystemLabel,document.getElementById('debugText'));
+var showsystem = document.getElementById('showSystem');
+showSystem.onchange = function(event){
+    var systemLogs = document.getElementsByClassName('systemMethodLog');
+    for(var i =0; i < systemLogs.length;i++){
+        systemLogs[i].style.display = this.checked ? 'block' : 'none';
+    }
+}
 for(var index=0; index < userDebugDivs.length; index++){
     //if(userDebugDivs[index].innderHTML.indexOf('BEAUTIFY') > -1)
+    if(userDebugDivs[index].innerHTML.indexOf('~^~') > -1){
+        userDebugDivs[index].innerHTML = sfdcObjectBeautify(userDebugDivs[index].innerHTML);
+    }
     userDebugDivs[index].innerHTML = js_beautify(userDebugDivs[index].innerHTML);
 }
-console.log(document.getElementsByClassName('debug')[0].innerHTML )//= js_beautify(document.getElementsByClassName('debug')[0].innerHTML);
 
 function escapeHtml(str) {
     var div = document.createElement('div');
     div.appendChild(document.createTextNode(str));
     return div.innerHTML;
 };
+
+function sfdcObjectBeautify(string){
+    string = string.split('~^~').join('\n');
+
+    return string.split('').reduce(function(prevSum,curChar,index,arr){
+        if(curChar == '{'){
+            return prevSum + '{"';
+        }
+        if(curChar == '}'){
+            return prevSum + '"}';
+        }
+        if(curChar == ','){
+            return prevSum + '","';
+        }
+        if(curChar == '='){
+            return prevSum + '":"';
+        }
+        return prevSum + curChar;
+    })
+}
