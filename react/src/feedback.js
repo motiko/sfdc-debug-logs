a5f1f1f2dd9edf1e45f77adb1fb0056e95d84a12
moti <moti.kor@gmail.com>
2017-11-25 09:54:40 +0700

    freactor message view out

--- react/src/feedback.js
+++ react/src/feedback.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import {List, ListItem} from 'material-ui/List';
+import {List, ListItem} from 'material-ui/List'
 import FlatButton from 'material-ui/FlatButton'
 import BackIcon from  'material-ui/svg-icons/navigation/arrow-back'
 import IconButton from 'material-ui/IconButton'
 import MessageIcon from 'material-ui/svg-icons/communication/message'
-import ReplyIcon from 'material-ui/svg-icons/content/reply'
 import MessageEdit from './components/message-edit'
+import MessageView from './components/message-view'
 import Dialog from 'material-ui/Dialog';
 
 const BASE_URL = "https://adbg.herokuapp.com"
@@ -69,7 +69,7 @@ export default class FeedbackPage extends React.Component {
       <List>
 
         {this.state.messages.map((m, i)=> (
-          <ViewMessage nested={false} message={m}
+          <MessageView nested={false} message={m}
               onReply={() => this.handleReply(m)} key={i}/>))}
       </List>
       <Dialog
@@ -79,28 +79,5 @@ export default class FeedbackPage extends React.Component {
       <MessageEdit onSubmit={this.sendMessage}/>
       </Dialog>
     </div>)
-  }
-}
-
-function ViewMessage(props){
-  const m = props.message
-  const bodyLines = m.body.split('\n')
-  const secondaryText = bodyLines.length > 1
-                         ? (<p> {bodyLines[1]} <br/> Sent By: {m.author}</p>)
-                         : `Sent By: ${m.author}`
-  const replyButton =  (<IconButton tooltip="Reply" onClick={props.onReply}><ReplyIcon/></IconButton>)
-  if(props.nested){
-    return (<ListItem primaryText={bodyLines[0]}
-        secondaryTextLines={2}
-        secondaryText={secondaryText} disabled={true} key={props.key}/>)
-  }else{
-    return (
-      <ListItem primaryText={bodyLines[0]} secondaryText={secondaryText}
-                disabled={true}
-                secondaryTextLines={2}
-                rightIconButton={replyButton}
-                open={true}
-                nestedItems={m.replies ?
-                m.replies.map((m,i) => ViewMessage({nested:true, message:m, key:i})) : []}/>)
   }
 }
