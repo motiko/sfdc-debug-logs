9e40a214c63b8ac679f591250501ef73f9a93737
moti <moti.kor@gmail.com>
2017-11-28 09:22:58 +0700

    polling for new msgs
    also keeping heroku alive

--- msg_watch/get_new.sh
+++ msg_watch/get_new.sh
@@ -1,12 +1,25 @@
 #/bin/sh
+NEW=`curl http://adbg.herokuapp.com/messages 2>/dev/null`
+if (( $? != 0)); then
+  exit $?
+fi
 touch new.json
 mv new.json last.json
-curl http://adbg.herokuapp.com/messages 2>/dev/null | jq . > new.json
+echo $NEW | jq . > new.json
 diff -q last.json new.json
 if (( $? != 0 )) ; then
   echo "New Message"
   # say "New Message"
-  git --no-pager diff --no-index --no-color -- new.json last.json | egrep "^- " | cut -c 2- | sed '$ s/,$//' | jq .body
+  RAW_DIFF=`git --no-pager diff --no-index --no-color -- new.json last.json`
+  PART=`echo $RAW_DIFF | egrep "^- " | cut -c 2- `
+  if ( echo $RAW_DIFF | grep replies ) ; then
+    echo {$PART} | jq .replies
+  else
+    echo $PART | sed '$ s/,$//' | jq .body
+  fi
+  TIME=`date +%s`
+  mkdir log/$TIME
+  cp *.json log/$TIME/
 else
   echo "No news"
 fi
